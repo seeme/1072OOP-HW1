e45d527d48c3517a9126dce67114697d597686b6
HsiMinChen <hmchen@mail.fcu.edu.tw>
2019-02-21 14:17:05 +0800

    Init project

---
+++ src/domo/Demo.java
@@ -0,0 +1,15 @@
+package domo;
+
+public class Demo {
+	
+	/**
+	 * This is a comment
+	 * @param args input from console
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+	}
+
+}
